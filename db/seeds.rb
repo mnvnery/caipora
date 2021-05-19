1e0bcbe0368d50251332e0f67892a40633248155
Maria Nery <67118326+mnvnery@users.noreply.github.com>
2021-05-19 22:56:35 +0100

    db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Trip.create(location: "Guatemala", start_date: Date.new(2021,7,23), end_date: Date.new(2021,7,30), price: "2000")
+Trip.create(location: "Amazónia", start_date: Date.new(2021,7,23), end_date: Date.new(2021,7,30), price: "2500")
